f52c6a19b1f8646f4497f375d8b09dfc592e5c1d
Gabriel <gabriel_3404@hotmail.com>
2020-06-30 19:36:51 -0300

    conexao com banco de dados corrigida

---
+++ Controllers/MoedasController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PesquisaCrypto.Models;
+
+namespace PesquisaCrypto.Controllers
+{
+    public class MoedasController : Controller
+    {
+        private readonly MoedasContexto _context;
+
+        public MoedasController(MoedasContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Moedas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Moedas.ToListAsync());
+        }
+
+        // GET: Moedas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var moedas = await _context.Moedas
+                .FirstOrDefaultAsync(m => m.MoedasId == id);
+            if (moedas == null)
+            {
+                return NotFound();
+            }
+
+            return View(moedas);
+        }
+
+        // GET: Moedas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Moedas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MoedasId,Nome,Quantidade")] Moedas moedas)
+        {
+            if (ModelState.IsValid)
+            {
+                moedas.Quantidade = 0;
+                _context.Add(moedas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(moedas);
+        }
+    }
+}
